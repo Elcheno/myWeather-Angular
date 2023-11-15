2fe45b503aa97773572194af8b9651200a6235a7
Elcheno <r.c.a2312@gmail.com>
2023-11-15 16:56:37 +0100

    add inject in myWeather service

--- src/app/services/my-weather.service.ts
+++ src/app/services/my-weather.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -7,9 +7,9 @@ import { Observable } from 'rxjs';
 })
 export class MyWeatherService {
 
-  constructor(
-    private http:HttpClient
-    ) { }
+  private http = inject(HttpClient);
+
+  constructor() { }
 
   getForeCast():Observable<any>{
    return this.http.get('http://api.weatherapi.com/v1/forecast.json?key=fc8c689bc9604eb0b1e113628231411&q=37.668335,-4.725080&days=3&aqi=no&alerts=no');
